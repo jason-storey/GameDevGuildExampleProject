9cea952235937d414de5e38874cd8af085140ca9
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-16 18:13:45 +0100

    chore: Adds Paginator Extension

--- PokemonApp/PokemonApp/EnumerableExtensions.cs
+++ PokemonApp/PokemonApp/EnumerableExtensions.cs
@@ -5,7 +5,14 @@ namespace PokemonApp
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<T> Paginated<T>(this IEnumerable<T> items,int pageSize, int page = 1) => 
-            items.Skip((page - 1) * pageSize).Take(pageSize);
+        public const int DEFAULT_PAGE_SIZE = 20;
+
+        public static IEnumerable<T> Paginated<T>(this IEnumerable<T> items,int pageSize, int page = 1)
+        {
+            if (items == null) return Enumerable.Empty<T>();
+            if (page <= 0) return items;
+            if (pageSize <= 0) pageSize = DEFAULT_PAGE_SIZE;
+            return items.Skip((page - 1) * pageSize).Take(pageSize);
+        }
     }
 }
--- PokemonApp/PokemonAppTests/EnumerableExtensions_Should.cs
+++ PokemonApp/PokemonAppTests/EnumerableExtensions_Should.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 using PokemonApp;
@@ -8,7 +9,36 @@ namespace PokemonAppTests
     public class EnumerableExtensions_Should
     {
         [Test]
-        public void When_collection_is_empty_return_empty() => 
+        public void When_collection_is_empty_return_empty() =>
             Enumerable.Empty<string>().Paginated(5, 10).Should().BeEmpty();
+
+        [Test]
+        public void When_collection_is_null_return_empty()
+        {
+            IEnumerable<string> collection = null;
+            collection.Paginated(1, 1).Should().BeEmpty();
+        }
+
+        [Test]
+        public void When_pageSize_is_less_than_zero_use_default()
+        {
+            var results = Enumerable.Range(0, 100).Paginated(-50);
+            results.Count().Should().Be(EnumerableExtensions.DEFAULT_PAGE_SIZE);
+        }
+
+        [Test]
+        public void When_page_is_negative_return_all()
+        {
+            var results = Enumerable.Range(0, 100).Paginated(20, -3);
+            results.Count().Should().Be(100);
+        }
+
+        [Test]
+        public void When_pageSize_is_greater_than_collection_return_all()
+        {
+            var results = Enumerable.Range(0, 100).Paginated(1000, -3);
+            results.Count().Should().Be(100);
+        }
+        
     }
 }
