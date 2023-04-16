3dad3665548a5dbf19039254a52e07de6ecf3859
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-16 17:57:09 +0100

    chore: Adds Unity Testing Project

---
+++ PokemonApp/PokemonApp/EnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonApp
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Paginated<T>(this IEnumerable<T> items,int pageSize, int page = 1) => 
+            items.Skip((page - 1) * pageSize).Take(pageSize);
+    }
+}
---
+++ PokemonApp/PokemonAppTests/EnumerableExtensions_Should.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using PokemonApp;
+namespace PokemonAppTests
+{
+    [TestFixture]
+    public class EnumerableExtensions_Should
+    {
+        [Test]
+        public void When_collection_is_empty_return_empty() => 
+            Enumerable.Empty<string>().Paginated(5, 10).Should().BeEmpty();
+    }
+}
