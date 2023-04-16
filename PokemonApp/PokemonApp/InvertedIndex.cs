47225fdddd62af84b50387f9ba67d9cb21619653
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-16 20:10:42 +0100

    feature: Adds InvertedIndex
    Adds a class to provide an inverted index cache, used for efficient lookups of tag lists

---
+++ PokemonApp/PokemonApp/InvertedIndex.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace PokemonApp
+{
+    public class InvertedIndex<T> : IEnumerable<T>
+    {
+        readonly Dictionary<string, HashSet<T>> _indexes;
+        readonly HashSet<T> _elements;
+        public InvertedIndex()
+        {
+            _elements = new HashSet<T>();
+            _indexes = new Dictionary<string, HashSet<T>>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        public IEnumerable<T> this[string key] => _indexes.TryGetValue(key, out var k) ? k : Enumerable.Empty<T>();
+        public int ItemCount => _elements.Count;
+        public int KeyCount => _indexes.Count;
+        public IEnumerable<string> Keys => _indexes.Keys;
+        public IEnumerable<T> Values => _elements;
+        public bool IsDirty { get; private set; }
+
+        public IEnumerable<T> WithAllOf(params string[] tags)
+        {
+            if (tags == null || tags.Length == 0) return Enumerable.Empty<T>();
+            var tagSet = new HashSet<string>(tags, StringComparer.OrdinalIgnoreCase);
+            string smallestTag = null;
+            int smallestTagItemCount = int.MaxValue;
+            foreach (string tag in tagSet)
+            {
+                if (_indexes.TryGetValue(tag, out var itemsWithTag))
+                {
+                    if (itemsWithTag.Count >= smallestTagItemCount) continue;
+                    smallestTag = tag;
+                    smallestTagItemCount = itemsWithTag.Count;
+                }
+                else
+                    return Enumerable.Empty<T>();
+            }
+
+            tagSet.Remove(smallestTag);
+            var results = new List<T>();
+            foreach (T item in _indexes[smallestTag])
+            {
+                bool hasAllTags = true;
+                foreach (string tag in tagSet)
+                {
+                    if (_indexes[tag].Contains(item)) continue;
+                    hasAllTags = false;
+                    break;
+                }
+
+                if (hasAllTags) results.Add(item);
+            }
+            return results;
+        }
+        
+        public IEnumerable<T> FindByQuery(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query)) return Enumerable.Empty<T>();
+            var includeTags = new List<string>();
+            var excludeTags = new List<string>();
+            var tokens = query.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var token in tokens)
+            {
+                if (token.StartsWith("!"))
+                    excludeTags.Add(token.Substring(1));
+                else
+                    includeTags.Add(token);
+            }
+
+            return _elements.Where(item =>
+            {
+                if (excludeTags.Any(tag => _indexes[tag].Contains(item)))
+                    return false;
+                return includeTags.Count == 0 || includeTags.Any(tag => _indexes[tag].Contains(item));
+            });
+        }
+        
+        public IEnumerable<T> FindByAnyTag(params string[] tags)
+        {
+            if (tags == null || tags.Length == 0) return Enumerable.Empty<T>();
+            string largestTag = null;
+            int largestTagItemCount = int.MinValue;
+            foreach (string tag in tags)
+            {
+                if (!_indexes.TryGetValue(tag, out var itemsWithTag)) continue;
+                if (itemsWithTag.Count <= largestTagItemCount) continue;
+                largestTag = tag;
+                largestTagItemCount = itemsWithTag.Count;
+            }
+            if (largestTag == null) return Enumerable.Empty<T>();
+            var results = new HashSet<T>(_indexes[largestTag]);
+            foreach (string tag in tags)
+            {
+                if (tag != largestTag && _indexes.TryGetValue(tag, out var itemsWithTag))
+                    results.UnionWith(itemsWithTag);
+            }
+            return results;
+        }
+        
+        public void Add(T item,bool dontDirty, params string[] labels)
+        {
+            var existingDirtyFlag = IsDirty;
+            Add(item,labels);
+            IsDirty = !dontDirty || existingDirtyFlag;
+        }
+        public void Add(T item,params string[] labels)
+        {
+            _elements.Add(item);
+            foreach (string label in labels)
+            {
+                if (string.IsNullOrWhiteSpace(label)) continue;
+                if (!_indexes.ContainsKey(label))
+                    _indexes[label] = new HashSet<T> { item };
+                else
+                    _indexes[label].Add(item);
+            }
+            IsDirty = true;
+        }
+        
+        public class JsonConverter : Newtonsoft.Json.JsonConverter
+        {
+            public override bool CanConvert(Type objectType) => objectType == typeof(InvertedIndex<T>);
+
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                var invertedIndex = (InvertedIndex<T>)value;
+                writer.WriteStartObject();
+                foreach (var key in invertedIndex.Keys)
+                {
+                    writer.WritePropertyName(key);
+                    writer.WriteStartArray();
+                    foreach (var val in invertedIndex[key]) 
+                        writer.WriteValue(val);
+                    writer.WriteEndArray();
+                }
+                writer.WriteEndObject();
+            }
+
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                if (reader.TokenType == JsonToken.Null)
+                    return null;
+
+                var invertedIndex = new InvertedIndex<T>();
+
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.PropertyName)
+                    {
+                        var label = reader.Value.ToString();
+                        reader.Read();
+
+                        if (reader.TokenType != JsonToken.StartArray) continue;
+                        while (reader.Read())
+                        {
+                            if (reader.TokenType == JsonToken.EndArray)
+                                break;
+
+                            T item = serializer.Deserialize<T>(reader);
+                            invertedIndex.Add(item,true, label);
+                        }
+                    }
+                    else if (reader.TokenType == JsonToken.EndObject)
+                    {
+                        break;
+                    }
+                }
+
+                return invertedIndex;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)_elements).GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ PokemonApp/PokemonAppTests/InvertedIndex_Should.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PokemonApp;
+
+namespace PokemonAppTests
+{
+    [TestFixture]
+    public class InvertedIndex_Should
+    {
+        [SetUp]
+        public void Setup()
+        {
+            _index = new InvertedIndex<string>();
+            _settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            };
+            _settings.Converters.Add(new InvertedIndex<string>.JsonConverter());
+        }
+
+        [Test]
+        public void When_created_has_no_elements()
+        {
+            _index.ItemCount.Should().Be(0);
+            _index.KeyCount.Should().Be(0);
+            _index.Keys.Should().BeEmpty();
+            _index.Values.Should().BeEmpty();
+            _index.IsDirty.Should().BeFalse();
+        }
+
+        [Test]
+        public void When_adding_element_mark_as_dirty()
+        {
+            _index.Add("Jason", "Awesome","Cool","Dude");
+            _index.ItemCount.Should().Be(1);
+            _index.KeyCount.Should().Be(3);
+            _index.Keys.Count().Should().Be(3);
+            _index.Values.Count().Should().Be(1);
+            _index.IsDirty.Should().BeTrue();
+        }
+
+        [Test]
+        public void When_indexing_empty_return_empty() => _index["Awesome"].Should().BeEmpty();
+
+        [Test]
+        public void When_indexing_with_matching_entry_return_matches()
+        {
+            _index.Add("Jason","programmer","dude","brunette");
+            _index.Add("Sarah","artist","dude","blonde");
+
+            _index["programmer"].Should().HaveCount(1);
+            _index["dude"].Should().HaveCount(2);
+            _index["artist"].Should().HaveCount(1);
+        }
+
+        [Test]
+        public void FindByAnyTag_ReturnsCorrectResults()
+        {
+            SetupTestData();
+            var result = _index.FindByAnyTag("tag2", "tag4");
+            result.Should().BeEquivalentTo(new List<string> { "Item1", "Item2", "Item3", "Item4" });
+        }
+        
+        
+        
+        
+        [Test]
+        public void When_withAllOf_returns_correct_items()
+        {
+            _index.Add("Jason","programmer","dude","brunette");
+            _index.Add("Sarah","artist","dude","blonde");
+            _index.Add("Alien","space","monoclops");
+
+            _index.WithAllOf("Programmer", "DUDe").Count().Should().Be(1);
+            _index.WithAllOf("dude").Count().Should().Be(2);
+            _index.WithAllOf("alien", "artist").Should().BeEmpty();
+        }
+
+        [Test]
+        public void When_querying_include_returns_correctly()
+        {
+            SetupTestData();
+            
+            var result = _index.FindByQuery("tag2 tag4");
+            result.Should().BeEquivalentTo(new List<string> { "Item1", "Item2", "Item3", "Item4" });
+        }
+
+        [Test]
+        public void When_querying_with_empty_returns_empty()
+        {
+            SetupTestData();
+            var results = _index.FindByQuery("");
+            results.Count().Should().Be(0);
+        }
+
+        void SetupTestData()
+        {
+            _index.Add("Item1", "tag1", "tag2");
+            _index.Add("Item2", "tag2");
+            _index.Add("Item3", "tag3", "tag4");
+            _index.Add("Item4", "tag4");
+        }
+
+        [Test]
+        public void When_querying_with_exclusion_and_include_returns()
+        {
+            SetupTestData();
+            var result = _index.FindByQuery("tag2 !tag1");
+            result.Should().BeEquivalentTo(new List<string> { "Item2" });
+        }
+        
+        [Test]
+        public void When_serializing_should_serialize_correctly()
+        {
+            _index.Add("Apple","Fruit","Red");
+            _index.Add("Banana","Fruit","Yellow");
+            _index.Add("Blueberry","Fruit","Blue");
+
+            var json = JsonConvert.SerializeObject(_index,_settings);
+            var restored = JsonConvert.DeserializeObject<InvertedIndex<string>>(json,_settings);
+            restored.ItemCount.Should().Be(_index.ItemCount);
+            restored.Keys.Count().Should().Be(_index.Keys.Count());
+            restored.Values.Count().Should().Be(_index.Values.Count());
+            restored.IsDirty.Should().BeFalse();
+        }
+
+        [Test]
+        public void When_adding_a_blank_label_do_not_add()
+        {
+            _index.Add("Apple","Fruit",string.Empty);
+            _index.ItemCount.Should().Be(1);
+        }
+        
+        [Test]
+        public void When_adding_with_dontDirty_flag_does_not_mark_dirty()
+        {
+            _index.Add("Apple",true,"Fruit","Nuts");
+            _index.IsDirty.Should().BeFalse();
+        }
+
+        
+        [Test]
+        public void When_foreaching_returns_values()
+        {
+            _index.Add("Apple","Fruit","Red");
+            _index.Add("Banana","Fruit","Yellow");
+            _index.Add("Blueberry","Fruit","Blue");
+
+            var expected = new List<string> { "Apple", "Banana", "Blueberry" };
+            var elements = _index.ToList();
+
+            elements.Should().BeEquivalentTo(expected);
+            var loopedElements = new List<string>();
+            foreach (var element in _index) 
+                loopedElements.Add(element);
+            loopedElements.Should().BeEquivalentTo(expected);
+        }
+
+        JsonSerializerSettings _settings;
+        InvertedIndex<string> _index;
+    }
+}
