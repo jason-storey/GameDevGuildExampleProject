4db0ea32b0df2aa1f165a5cd029a5cb982c12363
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-17 21:11:57 +0100

    chore: Adds a BKSearch tree and Levenstein distance
    adds support for spell checking

---
+++ PokemonApp/JCore/Collections/BKTree.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace JCore.Search
+{
+    public class BKTree
+    {
+        class Node
+        {
+            public string Word;
+            public Dictionary<int, Node> Children = new Dictionary<int, Node>();
+
+            public Node(string word) => Word = word;
+        }
+
+        Node root;
+
+        public void Insert(string word)
+        {
+            if (root == null)
+            {
+                root = new Node(word);
+                return;
+            }
+
+            var node = root;
+            while (true)
+            {
+                int d = Levenshtein.Calculate(node.Word, word);
+                if (d == 0) return;
+                if (!node.Children.TryGetValue(d, out var child))
+                {
+                    node.Children[d] = new Node(word);
+                    break;
+                }
+
+                node = child;
+            }
+        }
+
+        public List<string> Query(string word, int maxDistance)
+        {
+            var results = new List<string>();
+            var candidates = new Stack<Node>();
+            if (root != null) candidates.Push(root);
+
+            while (candidates.Count > 0)
+            {
+                var node = candidates.Pop();
+                int d = Levenshtein.Calculate(node.Word, word);
+                if (d <= maxDistance) results.Add(node.Word);
+                int minD = d - maxDistance, maxD = d + maxDistance;
+                foreach (var kv in node.Children)
+                {
+                    if (kv.Key >= minD && kv.Key <= maxD) candidates.Push(kv.Value);
+                }
+            }
+
+            return results;
+        }
+    }
+}
---
+++ PokemonApp/JCore/Search/Algorithms/Distances/Levenstein.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JCore.Search
+{
+    public static class Levenshtein
+    {
+        public static int Calculate(string s1, string s2) {
+            int m = s1.Length, n = s2.Length;
+            var d = new int[m + 1, n + 1];
+            for (int i = 0; i <= m; i++) d[i, 0] = i;
+            for (int j = 0; j <= n; j++) d[0, j] = j;
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    int cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[m, n];
+        }
+
+
+        public static bool HasLevenshteinLowEnough(this string s, string other, int target = 3) =>
+            Calculate(s, other) < target;
+        public static int ToLevenshtein(this string s, string other) => Calculate(s, other);
+    }
+}
