c18e9d94d18273e5f4f087f9f9d6809a3633e2b7
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-17 21:15:14 +0100

    chore: Creates a first pass of a complete search engine loop.
    This loop applies a series of transformations to autocomplete searches, correct spelling mistakes, add phonetic matches and then rank the results of the returned documents.

---
+++ PokemonApp/JCore/Search/SearchEngine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JCore.Collections;
+using JCore.Extensions;
+namespace JCore.Search
+{
+    public class SearchEngine
+    {
+        AutocompleteTrie _autoComplete;
+        BKTree _spellcheck;
+        HashedListDictionary<string> _phonetics;
+        BM25 _ranker;
+        public SearchEngine(List<string> documents)
+        {
+            _autoComplete = new AutocompleteTrie();
+            _spellcheck = new BKTree();
+            foreach (string document in documents)
+            {
+                _autoComplete.Insert(document);
+                _spellcheck.Insert(document);
+                _phonetics = Soundex.CreateCache(documents);
+            }
+        }
+
+        public IList<string> Search(string query, int maxEditDistance = 2)
+        {
+            var suggestions = _autoComplete.Search(query);
+            var correctedQueries = _spellcheck.Query(query, maxEditDistance);
+            foreach (var correctedSuggestions in correctedQueries.Select(correctedQuery => _autoComplete.Search(correctedQuery)))
+                suggestions.AddRange(correctedSuggestions);
+            suggestions.AddRange( _phonetics.GetValuesById(query.ToSoundex()));
+            suggestions.RemoveDuplicates();
+           return RankDocuments(query, suggestions);
+        }
+
+        List<string> RankDocuments(string query, List<string> documents) => 
+            new BM25(documents,new AdvancedTokenizer()).RankDocuments(query);
+    }
+}
