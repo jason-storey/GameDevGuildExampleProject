57c9e775eeb1e4b9f9b43ec3778c84eeeead882e
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-17 03:06:21 +0100

    chore: Adds Index conversion extensions
    adds ability to invert indexes

--- PokemonApp/PokemonApp/InvertedIndex/InvertedIndexExtensions.cs
+++ PokemonApp/PokemonApp/InvertedIndex/InvertedIndexExtensions.cs
@@ -18,7 +18,30 @@ public static class InvertedIndexExtensions
         foreach (var item in items) index.Add(item, labels?.Invoke(item));
         return index;
     }
-    
 
-    public static IEnumerable<T> Query<T>(this InvertedIndex<T> index,string query) => new InvertedIndexQuery<T>(index).ExecuteQuery(query);
+    public static InvertedIndex<string> ReverseIndex(this InvertedIndex<string> index)
+    {
+        var dict = index.ToDictionary();
+        var newIndex = new InvertedIndex<string>();
+        foreach (var kvp in dict)
+        {
+            foreach (var innerValue in kvp.Value) 
+                newIndex.Add(innerValue, kvp.Key);
+        }
+        return newIndex;
+    }
+
+    public static InvertedIndex<T> ToIndex<T>(this Dictionary<T, List<string>> dictionary)
+    {
+        var index = new InvertedIndex<T>();
+        foreach (var kvp in dictionary) 
+            index.Add(kvp.Key, kvp.Value.ToArray());
+        return index;
+    }
+
+    public static IEnumerable<T> Query<T>(this InvertedIndex<T> index,string query,InvertedIndex<string> aliases) => 
+        string.IsNullOrWhiteSpace(query) ? index : new InvertedIndexQuery<T>(index).ExecuteQuery(query,(token)=> aliases.ContainsKey(token) ? aliases[token].First() : token);
+
+    public static IEnumerable<T> Query<T>(this InvertedIndex<T> index,string query,Func<string,string> beforeTokenProcess = null) => 
+        string.IsNullOrWhiteSpace(query) ? index : new InvertedIndexQuery<T>(index).ExecuteQuery(query,beforeTokenProcess);
 }
