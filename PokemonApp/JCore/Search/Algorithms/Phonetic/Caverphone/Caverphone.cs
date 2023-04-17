87b6fcc6746c63e6b89299e35466694dad0d5586
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-17 15:23:33 +0100

    chore: Adds Caverphone Phonetic Algorithm

---
+++ PokemonApp/JCore/Search/Algorithms/Phonetic/Caverphone/Caverphone.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace JCore.Search.Algorithms
+{
+    public static class Caverphone
+    {
+        public static string Generate(string word)
+        {
+            if (string.IsNullOrEmpty(word)) return "";
+            word = word.ToUpper();
+            word = Regex.Replace(word, "[AEIOU]", "A");
+            word = word
+                .Replace("B", "P")
+                .Replace("C", "K")
+                .Replace("D", "T")
+                .Replace("F", "P")
+                .Replace("G", "K")
+                .Replace("H", "A")
+                .Replace("J", "Y")
+                .Replace("L", "R")
+                .Replace("M", "N")
+                .Replace("Q", "K")
+                .Replace("V", "P")
+                .Replace("W", "A")
+                .Replace("X", "K")
+                .Replace("Z", "S");
+            var sb = new StringBuilder();
+            sb.Append(word[0]);
+            for (int i = 1; i < word.Length; i++)
+                if (word[i] != word[i - 1])
+                    sb.Append(word[i]);
+            word = sb.ToString();
+            if (word.Length > 10) word = word.Substring(0, 10);
+            else if (word.Length < 10) word = word.PadRight(10, 'A');
+            return word;
+        }
+        
+        public static bool Match(this string word1, string word2) => Generate(word1) == Generate(word2);
+        public static string ToMetaphone(this string s) => Generate(s);
+
+        public static bool MatchesAsCaverphone(this string s, string other)
+        {
+            if (string.IsNullOrWhiteSpace(s) || string.IsNullOrWhiteSpace(other)) return false;
+            return Generate(s) == Generate(other);
+        }
+    }
+}
---
+++ PokemonApp/JCore/Search/Algorithms/Phonetic/Caverphone/CaverphoneAlgorithm.cs
@@ -0,0 +1,9 @@
+﻿using JCore.Common;
+
+namespace JCore.Search.Algorithms
+{
+    public class CaverphoneAlgorithm : CachedStringAlgorithm
+    {
+        public CaverphoneAlgorithm(IRepository<string> cache) : base(cache, Caverphone.Generate) { }
+    }
+}
