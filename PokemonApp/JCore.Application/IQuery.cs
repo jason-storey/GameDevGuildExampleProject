32b2154d385825ea555ca915d467235593e59d14
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-18 11:13:10 +0100

    chore: Adds Application Domain Utility Project
    There are aspects of an application that are shared across most apps. the ideas of selecting elements and displaying them. handling errors bubbling up to the ui, applying rules for validating objects before saving them. Creating new resources etc.
    
    These are not app specific and can be shared.

---
+++ PokemonApp/JCore.Application/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JCore.Application
+{
+    public interface IQuery<T>
+    {
+        Task<IEnumerable<T>> Execute();
+    }
+}
