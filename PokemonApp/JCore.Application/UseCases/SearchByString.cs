61238f8f4b2de2e376ff0a5c4a5a358d70e45f2b
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-18 18:17:42 +0100

    feat: Modified string search behaviour
    added some additional validation and order of operation changes to the search by string presenter

--- PokemonApp/JCore.Application/UseCases/SearchByString.cs
+++ PokemonApp/JCore.Application/UseCases/SearchByString.cs
@@ -1,32 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using JCore.Application.Presenters;
 using JCore.Application.Views;
-using static JCore.Application.MessageUtilities;
-
+using static JCore.Application.UseCaseMessages;
 namespace JCore.Application.UseCases
 {
     public class SearchByString<T> : StringSearchPresenter
     {
-        readonly IQueryFactory<T> _queryFactory;
+        #region plumbing
+
+        readonly IQueryFactory<T> _query;
         readonly StringSearchView<T> _view;
 
-        public SearchByString(IQueryFactory<T> queryFactory,StringSearchView<T> view)
+        public SearchByString(IQueryFactory<T> query,StringSearchView<T> view)
         {
-            _queryFactory = queryFactory;
+            _query = query;
             _view = view;
         }
-        
+
+        public bool AttemptEmptySearchAnyway { get; set; } = true;
+
+        #endregion
+
+        public View View => _view;
 
         public async void PerformSearch()
         {
             try
             {
-                _view.Results = await _queryFactory.Search(_view.Search).Execute();
+                var search = _view.Search;
+                if (string.IsNullOrWhiteSpace(search))
+                { 
+                    Say(EMPTY_SEARCH_STRING);
+                    
+                    if (AttemptEmptySearchAnyway)
+                        await UpdateUiWithResults(string.Empty);
+                    else 
+                        ClearSearchResults();
+                    
+                    return;
+                }
+                
+                await UpdateUiWithResults(search);
             }
             catch (Exception ex)
             {
-                _view.SendMessage(FromError(ex));
+               Say(INTERNAL_ERROR,ex);
             }
         }
+
+        void ClearSearchResults()
+        {
+            _view.Results = new List<T>();
+        }
+        
+        async Task UpdateUiWithResults(string query)
+        {
+            var results = (await _query.Search(query).Execute()).ToList();
+            if (results.Count <= 0) 
+               Say(NO_RESULTS);
+            _view.Results = results;
+        }
+
+        #region Utility
+
+        void Say(string message, object context = null) => _view.SendMessage(Message.Say(message, context));
+        
+
+        #endregion
     }
 }
--- PokemonApp/JCore.Application/Views/StringSearchView.cs
+++ PokemonApp/JCore.Application/Views/StringSearchView.cs
@@ -8,5 +8,6 @@ namespace JCore.Application.Views
 
         IEnumerable<T> Results { set; }
 
+        bool ClearSearchAfterResultsReturned { get; set; }
     }
 }
