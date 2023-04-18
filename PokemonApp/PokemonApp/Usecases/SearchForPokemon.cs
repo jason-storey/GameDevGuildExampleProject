75ea0553d8a2826e4ed34ff9ff8d2ab9b926079b
Jason Storey <jasonstoreyireland@gmail.com>
2023-04-18 12:35:36 +0100

    chore: Rebuild Application Tests

---
+++ PokemonApp/PokemonApp/Usecases/SearchForPokemon.cs
@@ -0,0 +1,13 @@
+﻿using JCore.Application;
+using JCore.Application.UseCases;
+using JCore.Application.Views;
+
+namespace PokemonApp
+{
+    public class SearchForPokemon : SearchByString<Pokemon>
+    {
+        public SearchForPokemon(IQueryFactory<Pokemon> queryFactory, StringSearchView<Pokemon> view) : base(queryFactory, view)
+        {
+        }
+    }
+}
